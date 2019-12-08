d9b267b7c2359f9f7400727449ec2760a884563a
jaesung lim <lim4057@naver.com>
2019-12-08 16:22:32 +0900

    first commit

---
+++ src/main/java/org/themselves/alber/AlberApplication.java
@@ -0,0 +1,13 @@
+package org.themselves.alber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/themselves/alber/AlberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.themselves.alber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
