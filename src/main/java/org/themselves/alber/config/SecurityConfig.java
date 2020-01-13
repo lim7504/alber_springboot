0f9140fc6a3931b8073c3f3621467909d0d75b47
jslim <lim4057@naver.com>
2020-01-13 09:56:10 +0900

    test1

--- src/main/java/org/themselves/alber/config/SecurityConfig.java
+++ src/main/java/org/themselves/alber/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
+
     }
 
     @Override // ignore check swagger resource
