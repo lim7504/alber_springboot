a0cb75b2348cb8b3214f8b2cd2d9e1efe35d8204
임재성 <imjaeseong@imjaeseong-ui-MacBookPro.local>
2020-01-28 00:13:44 +0900

    wastebasketContoroller Engineering ~ing

--- src/main/java/org/themselves/alber/controller/wastebasket/WastebasketController.java
+++ src/main/java/org/themselves/alber/controller/wastebasket/WastebasketController.java
@@ -4,18 +4,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pin")
+@RequestMapping("/wastebasket")
 public class WastebasketController {
 
     /**
-     * pin 등록
+     * 쓰레기통 등록
      * 쓰레기통과 이미지정보를 함께 등록
+     * pin도 함께 등록
+     */
+
+    /**
+     * 쓰레기통 보기(상세)
+     * 아무나 가능~~
+     * 댓글도 한번에 불러오기 등록된 순서 내림차순
+     */
+
+    /**
+     * 주변 500m에 있는 쓰레기통 보기
+     * 위도 경도를 받아와야 함
+     * 500m 계산 법? 위도 경도로부터 반지름 500m 이내 둘레의 쓰레기통 검색 로직
+     * 간이 정보, 사진
+     */
+
+    /**
+     * 쓰레기통 수정
+     * 이미지 수정가능
+     * 본인이 작성하지 않은 댓글은 수정 불가
+     * pin을 등록한 유저만이 수정가능
+     */
+
+    /**
+     * 쓰레기통 삭제
+     * 댓글이 달리지 않은 쓰레기통만 삭제
+     * pin을 등록한 유저만 삭제가능
      */
 
 
     /**
-     * pin 삭제
-     *
+     * 쓰레기통 댓글 등록
      */
+
+    /**
+     * 쓰레기통 댓글 수정
+     */
+
+    /**
+     * 쓰레기통 댓글 삭제
+     */
+
+
 
 }
