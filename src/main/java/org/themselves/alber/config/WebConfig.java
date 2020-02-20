363e9688a5a592fae801ca74e586bafaf8a3ab7c
jslim <lim4057@naver.com>
2020-02-20 17:50:01 +0900

    cors method add

--- src/main/java/org/themselves/alber/config/WebConfig.java
+++ src/main/java/org/themselves/alber/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.themselves.alber.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name());
     }
 }
 
