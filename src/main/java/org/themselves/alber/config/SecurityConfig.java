5024d3685e3823afb50dbfee42504f03f1455e0c
jaesung lim <lim4057@naver.com>
2020-02-24 22:44:03 +0900

    modify cors security config

--- src/main/java/org/themselves/alber/config/SecurityConfig.java
+++ src/main/java/org/themselves/alber/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -31,11 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/","/hello").permitAll()
                 .mvcMatchers(HttpMethod.POST,"/users","/users/test","/session").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin().disable();
         http.httpBasic().disable();
         http.csrf().disable();
+        http.cors();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
