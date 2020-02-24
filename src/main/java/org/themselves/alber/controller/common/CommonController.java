ffed1f9337b516760e34974dd8e7f32b8e07a5da
jaesung lim <lim4057@naver.com>
2020-02-24 22:17:03 +0900

    fix config syntax error

--- src/main/java/org/themselves/alber/controller/common/CommonController.java
+++ src/main/java/org/themselves/alber/controller/common/CommonController.java
@@ -1,34 +1,34 @@
-//package org.themselves.alber.controller.common;
-//
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//import org.themselves.alber.config.response.ResponseContent;
-//import org.themselves.alber.config.response.StatusCode;
-//import org.themselves.alber.service.CommonService;
-//
-//@RestController @RequiredArgsConstructor
-//@RequestMapping("/common")
-//@Api(description = "공통")
-//public class CommonController {
-//
-//    private final CommonService commonService;
-//
-//    @PostMapping(value = "/image", consumes = "multipart/form-data; charset=UTF-8", produces = "application/json; charset=UTF-8", headers = "X-AUTH-TOKEN")
-//    @ApiOperation(value = "이미지 등록")
-//    public ResponseEntity<ResponseContent<ImageDto>> addImage(
-//              @RequestParam("file") MultipartFile file) {
-//
-//        Long imageId = commonService.imageFileUpload(file);
-//        ImageDto imageDto = new ImageDto();
-//        imageDto.setImageId(imageId);
-//
-//        return ResponseEntity
-//                .ok()
-//                .body(new ResponseContent<>(StatusCode.SUCCESS, imageDto));
-//    }
-//
-//}
+package org.themselves.alber.controller.common;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.themselves.alber.config.response.ResponseContent;
+import org.themselves.alber.config.response.StatusCode;
+import org.themselves.alber.service.CommonService;
+
+@RestController @RequiredArgsConstructor
+@RequestMapping("/common")
+@Api(description = "공통")
+public class CommonController {
+
+    private final CommonService commonService;
+
+    @PostMapping(value = "/image", consumes = "multipart/form-data; charset=UTF-8", produces = "application/json; charset=UTF-8", headers = "X-AUTH-TOKEN")
+    @ApiOperation(value = "이미지 등록")
+    public ResponseEntity<ResponseContent<ImageDto>> addImage(
+              @RequestParam("file") MultipartFile file) {
+
+        Long imageId = commonService.imageFileUpload(file);
+        ImageDto imageDto = new ImageDto();
+        imageDto.setImageId(imageId);
+
+        return ResponseEntity
+                .ok()
+                .body(new ResponseContent<>(StatusCode.SUCCESS, imageDto));
+    }
+
+}
